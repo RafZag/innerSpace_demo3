5ccffe36029f6750e6f686f95cb30d74af86a955
Rafal <zagorny.rafal@gmail.com>
2022-03-03 18:32:18 +0100

    particlObject class

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.132.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/loaders/GLTFLoader.js";
 import { GUI } from "https://cdn.skypack.dev/three@0.137.0/examples/jsm/libs/lil-gui.module.min.js";
 import Stats from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/libs/stats.module.js";
-import { MeshSurfaceSampler } from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/math/MeshSurfaceSampler.js";
-// import { VertexNormalsHelper } from 'https://cdn.skypack.dev/three@0.132.0/examples/jsm/helpers/VertexNormalsHelper.js';
+import { particleObject } from "/particleObject.js";
 
 let camera, scene, renderer, stats, controls;
-let particles;
 let spaceParticles;
 let spaceVertices = [];
 
-let vertices = [];
-let surfaceVerts = [];
-let partColors = [];
-let sizes = [];
-let geometry = new THREE.BufferGeometry();
-let shaderMaterial;
+let classTest;
 const meshURL = "gltf/human.glb";
-const gltfLoader = new GLTFLoader();
-const MAX_PARTICLES = 500000;
-const MAX_SIZE = 6;
-let uniformsValues;
+
 let animDirection = true;
 let lastZoom;
 
 let viewportSurfaceArea = window.innerWidth * window.innerHeight * 0.0000001;
 
 const colorPallete = {
-  color1: 0x69bc9f,
+  color1: 0x4fcfae,
   color2: 0x74d5a7,
   color3: 0x84d6cd,
   color4: 0x9ce5f0,
@@ -37,12 +26,6 @@ const colorPallete = {
 
 const params = {
   backgroundColor: 0xdfe9f2,
-  particleColor: 0x4fcfae,
-  particleCount: 80000,
-  particleSize: 1.25,
-  particleSizeVariation: 0.025,
-  particlesWobble: 0.08,
-  wobbleSpeed: 0.03,
 };
 
 const tweenParams = {
@@ -69,12 +52,6 @@ let buildTween = new TWEEN.Tween(tweenParams)
   .onUpdate(() => {
     buildAnimation();
   });
-
-// ------------------ Sampler --------------------
-
-let surfaceMesh;
-let sampler;
-const _position = new THREE.Vector3();
 
 init();
 animate();
@@ -114,9 +91,9 @@ function init() {
   controls.target = new THREE.Vector3(0, 18, 0);
   controls.enableDamping = true;
   lastZoom = 0;
-  controls.addEventListener("change", () => {
-    zoomResample();
-  });
+  // controls.addEventListener("change", () => {
+  //   zoomResample();
+  // });
   // controls.autoRotate = true;
   // controls.autoRotateSpeed = 0.5;
   window.addEventListener("resize", onWindowResize);
@@ -126,20 +103,23 @@ function init() {
   // stats = new Stats();
   // document.body.appendChild(stats.dom);
 
-  const gui = new GUI();
-  const folder1 = gui.addFolder("Particles");
-  folder1.add(params, "particleCount", 1000, 500000).onChange(resample).listen();
-  folder1.add(params, "particleSize", 0, 5).onChange(changeParticleSize).listen();
-  folder1.add(params, "particleSizeVariation", 0, 1, 0.01).onChange(changeParticleSize);
-  folder1.add(params, "particlesWobble", 0, 1, 0.01);
-  folder1.add(params, "wobbleSpeed", 0, 2, 0.01);
-  gui.add(tweenParams, "startAnim");
+  // const gui = new GUI();
+  // const folder1 = gui.addFolder("Particles");
+  // folder1.add(params, "particleCount", 1000, 500000).onChange(resample).listen();
+  // folder1.add(params, "particleSize", 0, 5).onChange(changeParticleSize).listen();
+  // folder1.add(params, "particleSizeVariation", 0, 1, 0.01).onChange(changeParticleSize);
+  // folder1.add(params, "particlesWobble", 0, 1, 0.01);
+  // folder1.add(params, "wobbleSpeed", 0, 2, 0.01);
+  // gui.add(tweenParams, "startAnim");
 
-  gui.close();
+  // gui.close();
 
   buildSpaceParticles();
-  buildParticles();
-  zoomResample();
+  // buildParticles();
+  // zoomResample();
+
+  classTest = new particleObject(scene, "gltf/human.glb", colorPallete.color1);
+  // console.log(classTest);
 }
 
 //---------------- Animate --------------------------
@@ -157,20 +137,19 @@ function animate(time) {
 function render() {
   spaceParticles.rotation.y += 0.0002;
   // spaceParticles.rotation.x += 0.0002;
-  uniformsValues["time"].value = performance.now() * params.wobbleSpeed * 0.0000001;
-  uniformsValues["wobble"].value = params.particlesWobble;
-  // uniformsValues.needsUpdate = true;
+  classTest.update();
   renderer.render(scene, camera);
 }
 //---------------- Window resize --------------------------
 
 function onWindowResize() {
   viewportSurfaceArea = window.innerWidth * window.innerHeight * 0.0000001;
-  changeParticleSize();
+  // changeParticleSize();
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
+
 function buildSpaceParticles() {
   let spaceParticlesGeo = new THREE.BufferGeometry();
   const sprite = new THREE.TextureLoader().load("img/pointSprite.png");
@@ -193,128 +172,23 @@ function buildSpaceParticles() {
     alphaTest: 0.1,
     transparent: true,
   });
-  mat.color.set(params.particleColor);
+  mat.color.set(colorPallete.color1);
 
   spaceParticles = new THREE.Points(spaceParticlesGeo, mat);
   scene.add(spaceParticles);
 }
 
-function buildParticles() {
-  const pc = new THREE.Color(params.particleColor);
-  geometry = new THREE.BufferGeometry();
+// function buildAnimation() {
+//   const positions = particles.geometry.attributes.position.array;
 
-  for (let j = 0; j < MAX_PARTICLES; j++) {
-    vertices.push(0, 0, 0);
-    partColors.push(pc.r, pc.g, pc.b);
-    sizes.push(params.particleSize * viewportSurfaceArea + (Math.random() - 0.5) * 2 * params.particleSizeVariation);
-  }
+//   let trans = tweenParams.transition;
+//   const originPoint = new THREE.Vector3(0);
 
-  geometry.setAttribute("color", new THREE.Float32BufferAttribute(partColors, 3));
-  geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
-  geometry.setAttribute("size", new THREE.Float32BufferAttribute(sizes, 1).setUsage(THREE.DynamicDrawUsage));
+//   for (let i = 0; i < surfaceVerts.length; i++) {
+//     positions[i * 3] = surfaceVerts[i].x + (originPoint.x - surfaceVerts[i].x) * trans;
+//     positions[i * 3 + 1] = surfaceVerts[i].y + (originPoint.y - surfaceVerts[i].y) * trans;
+//     positions[i * 3 + 2] = surfaceVerts[i].z + (originPoint.z - surfaceVerts[i].z) * trans;
+//   }
 
-  geometry.setDrawRange(0, params.particleCount);
-
-  uniformsValues = {
-    rimColor: { value: new THREE.Color("rgb(255, 255, 255)") },
-    time: { value: 0.0 },
-    wobble: { value: params.particlesWobble },
-  };
-
-  shaderMaterial = new THREE.ShaderMaterial({
-    uniforms: uniformsValues,
-    vertexShader: document.getElementById("vertexshader").textContent,
-    fragmentShader: document.getElementById("fragmentshader").textContent,
-    // blending: THREE.AdditiveBlending,
-    // depthWrite: false,
-    depthTest: true,
-    transparent: true,
-    vertexColors: true,
-  });
-
-  particles = new THREE.Points(geometry, shaderMaterial);
-  particles.frustumCulled = false; ////  object visibility fixed
-  scene.add(particles);
-  loadMesh();
-}
-
-function loadMesh() {
-  gltfLoader.load(
-    meshURL,
-    function (gltf) {
-      // scene.add(gltf.scene);
-      surfaceMesh = gltf.scene.children[0]; // Object
-      // console.log(gltf);
-      sampler = new MeshSurfaceSampler(surfaceMesh).setWeightAttribute("color").build();
-      sampleSurface();
-    },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    function (error) {
-      console.log("An error happened " + error);
-    }
-  );
-}
-
-function sampleSurface() {
-  for (let i = 0; i < MAX_PARTICLES; i++) {
-    sampler.sample(_position);
-    let v = new THREE.Vector3(_position.x, _position.y, _position.z);
-    surfaceVerts.push(v);
-  }
-  surfaceScatter();
-}
-
-function surfaceScatter() {
-  const positions = particles.geometry.attributes.position.array;
-  let index = 0;
-
-  for (let i = 0; i < MAX_PARTICLES; i++) {
-    positions[index++] = surfaceVerts[i].x;
-    positions[index++] = surfaceVerts[i].y;
-    positions[index++] = surfaceVerts[i].z;
-  }
-  particles.geometry.attributes.position.needsUpdate = true;
-  zoomResample();
-}
-
-function resample() {
-  geometry.setDrawRange(0, params.particleCount);
-  geometry.attributes.position.needsUpdate = true;
-}
-
-function zoomResample() {
-  let dist = controls.getDistance();
-  if (Math.abs(lastZoom - dist) > 1) {
-    params.particleCount = (MAX_PARTICLES * 50) / (dist * dist);
-    resample();
-    params.particleSize = (MAX_SIZE * dist) / 70;
-    changeParticleSize();
-    lastZoom = dist;
-  }
-}
-
-function changeParticleSize() {
-  const sizes = geometry.attributes.size.array;
-
-  for (let i = 0; i < geometry.attributes.size.array.length; i++) {
-    sizes[i] = params.particleSize * viewportSurfaceArea + (Math.random() - 0.5) * 2 * params.particleSizeVariation;
-  }
-  geometry.attributes.size.needsUpdate = true;
-}
-
-function buildAnimation() {
-  const positions = particles.geometry.attributes.position.array;
-
-  let trans = tweenParams.transition;
-  const originPoint = new THREE.Vector3(0);
-
-  for (let i = 0; i < surfaceVerts.length; i++) {
-    positions[i * 3] = surfaceVerts[i].x + (originPoint.x - surfaceVerts[i].x) * trans;
-    positions[i * 3 + 1] = surfaceVerts[i].y + (originPoint.y - surfaceVerts[i].y) * trans;
-    positions[i * 3 + 2] = surfaceVerts[i].z + (originPoint.z - surfaceVerts[i].z) * trans;
-  }
-
-  particles.geometry.attributes.position.needsUpdate = true;
-}
+//   particles.geometry.attributes.position.needsUpdate = true;
+// }
--- particleObject.js
+++ particleObject.js
@@ -1,7 +1,173 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.132.0/build/three.module.js";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/loaders/GLTFLoader.js";
+import { MeshSurfaceSampler } from "https://cdn.skypack.dev/three@0.132.0/examples/jsm/math/MeshSurfaceSampler.js";
+
 class particleObject {
-  positon = { x: 0, y: 0, z: 0 };
-  rotation = { x: 0, y: 0, z: 0 };
-  visible = false;
-  particles;
-  vertices = [];
+  scene;
+  positon = new THREE.Vector3();
+  rotation = new THREE.Vector3();
+
+  visible = true;
+  particles; // THREE.Points(); - main object added to scene
+  vertices = []; // particles verts
+  surfaceVerts = [];
+  partColors = [];
+  sizes = [];
+  color;
+
+  modelURL;
+  gltfLoader = new GLTFLoader();
+  geometry = new THREE.BufferGeometry();
+
+  surfaceMesh;
+  sampler;
+  _position = new THREE.Vector3();
+
+  uniformsValues;
+
+  params = {
+    particleColor: 0x4fcfae,
+    particleCount: 80000,
+    particleSize: 0.25,
+    particleSizeVariation: 0.025,
+    particlesWobble: 0.08,
+    wobbleSpeed: 0.03,
+  };
+
+  MAX_PARTICLES = 500000;
+  MAX_SIZE = 6;
+
+  constructor(scene, model, col) {
+    this.scene = scene;
+    this.params.particleColor = new THREE.Color(col);
+    this.modelURL = model;
+    this.buildParticles();
+  }
+
+  buildParticles() {
+    const pc = new THREE.Color(this.params.particleColor);
+
+    for (let j = 0; j < this.MAX_PARTICLES; j++) {
+      this.vertices.push(0, 0, 0);
+      this.partColors.push(pc.r, pc.g, pc.b);
+      this.sizes.push(this.params.particleSize);
+    }
+
+    this.geometry.setAttribute("color", new THREE.Float32BufferAttribute(this.partColors, 3));
+    this.geometry.setAttribute("position", new THREE.Float32BufferAttribute(this.vertices, 3));
+    this.geometry.setAttribute("size", new THREE.Float32BufferAttribute(this.sizes, 1).setUsage(THREE.DynamicDrawUsage));
+
+    this.geometry.setDrawRange(0, this.params.particleCount);
+
+    this.uniformsValues = {
+      rimColor: { value: new THREE.Color("rgb(255, 255, 255)") },
+      time: { value: 0.0 },
+      wobble: { value: this.params.particlesWobble },
+    };
+
+    const shaderMaterial = new THREE.ShaderMaterial({
+      uniforms: this.uniformsValues,
+      vertexShader: document.getElementById("vertexshader").textContent,
+      fragmentShader: document.getElementById("fragmentshader").textContent,
+      // blending: THREE.AdditiveBlending,
+      // depthWrite: false,
+      depthTest: true,
+      transparent: true,
+      vertexColors: true,
+    });
+
+    const sprite = new THREE.TextureLoader().load("img/pointAlpha.png");
+
+    let mat = new THREE.PointsMaterial({
+      size: 0.1,
+      sizeAttenuation: true,
+      // map: sprite,
+      // blending: THREE.AdditiveBlending,
+      depthTest: false,
+      alphaTest: 0.1,
+      alphaMap: sprite,
+      transparent: true,
+    });
+    mat.color.set(this.params.particleColor);
+
+    this.particles = new THREE.Points(this.geometry, shaderMaterial);
+    this.particles.frustumCulled = false; ////  object visibility fixed
+
+    this.scene.add(this.particles);
+    this.loadMesh(this.modelURL);
+  }
+
+  loadMesh(url) {
+    this.gltfLoader.load(
+      url,
+      function (gltf) {
+        // this.scene.add(gltf.scene);
+        this.surfaceMesh = gltf.scene.children[0]; // Object
+        // console.log(gltf);
+        this.sampler = new MeshSurfaceSampler(this.surfaceMesh).setWeightAttribute("color").build();
+        this.sampleSurface();
+      }.bind(this),
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      function (error) {
+        console.log("An error happened " + error);
+      }
+    );
+  }
+
+  sampleSurface() {
+    for (let i = 0; i < this.MAX_PARTICLES; i++) {
+      this.sampler.sample(this._position);
+      let v = new THREE.Vector3(this._position.x, this._position.y, this._position.z);
+      this.surfaceVerts.push(v);
+    }
+    this.surfaceScatter();
+  }
+
+  surfaceScatter() {
+    const positions = this.particles.geometry.attributes.position.array;
+    let index = 0;
+
+    for (let i = 0; i < this.MAX_PARTICLES; i++) {
+      positions[index++] = this.surfaceVerts[i].x;
+      positions[index++] = this.surfaceVerts[i].y;
+      positions[index++] = this.surfaceVerts[i].z;
+    }
+    this.particles.geometry.attributes.position.needsUpdate = true;
+    //zoomResample();
+  }
+
+  resample() {
+    this.geometry.setDrawRange(0, this.params.particleCount);
+    this.geometry.attributes.position.needsUpdate = true;
+  }
+
+  //   zoomResample() {
+  //     let dist = controls.getDistance();
+  //     if (Math.abs(lastZoom - dist) > 1) {
+  //       params.particleCount = (MAX_PARTICLES * 50) / (dist * dist);
+  //       resample();
+  //       params.particleSize = (MAX_SIZE * dist) / 70;
+  //       changeParticleSize();
+  //       lastZoom = dist;
+  //     }
+  //   }
+
+  //   changeParticleSize() {
+  //     const sizes = this.geometry.attributes.size.array;
+  //     for (let i = 0; i < geometry.attributes.size.array.length; i++) {
+  //       sizes[i] = this.params.particleSize * viewportSurfaceArea + (Math.random() - 0.5) * 2 * this.params.particleSizeVariation;
+  //     }
+  //     geometry.attributes.size.needsUpdate = true;
+  //   }
+
+  update() {
+    // console.log(performance.now());
+    this.uniformsValues["time"].value = performance.now() * this.params.wobbleSpeed * 0.0000001;
+    this.uniformsValues["wobble"].value = this.params.particlesWobble;
+    this.uniformsValues.needsUpdate = true;
+  }
 }
+
+export { particleObject };
